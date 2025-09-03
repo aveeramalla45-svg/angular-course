d6e161a04ae98692ae0a86d86553fff686890b65
AkhilVeeramalla <aveeramalla45@gmail.com>
2025-09-04 00:14:30 +0530

    Create new component Create Component inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CourseCardComponent } from "./course-card/course-card.component";
+import {COURSES} from '../db-data';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CourseCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -17,5 +19,9 @@ onCongratsClick(){
 onKeyUp(newTitle : string){
   this.title = newTitle;
 }
+
+coreCourse = COURSES[0];
+rxjsCourse = COURSES[1];
+ngrxCourse = COURSES[2];
 
 }
---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'app-course-card',
+  imports: [],
+  templateUrl: './course-card.component.html',
+  styleUrl: './course-card.component.css'
+})
+export class CourseCardComponent {
+  @Input()
+  title!: string;
+
+  @Input()
+  course!: Course;
+}
