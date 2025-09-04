0c510b6c829356930d6ab22760f9101c27af59b0
AkhilVeeramalla <aveeramalla45@gmail.com>
2025-09-04 16:05:28 +0530

    Output decorator Custom events and event emitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CourseCardComponent } from "./course-card/course-card.component";
 import {COURSES} from '../db-data';
+import { Course } from './model/course';
 
 @Component({
   selector: 'app-root',
@@ -23,5 +24,9 @@ onKeyUp(newTitle : string){
 coreCourse = COURSES[0];
 rxjsCourse = COURSES[1];
 ngrxCourse = COURSES[2];
+
+onCourseSelected(course:Course){
+    console.log("app component - click event bubbled...", course);
+}
 
 }
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Course } from '../model/course';
 
 @Component({
@@ -13,4 +13,13 @@ export class CourseCardComponent {
 
   @Input()
   course!: Course;
+
+  @Output('courseSelected')
+  courseEmitter = new EventEmitter<Course>();
+
+  onCourseViewed(){
+    console.log("card component - button clicked...");
+
+    this.courseEmitter.emit(this.course);
+  }
 }
