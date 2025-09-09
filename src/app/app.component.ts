1ad3b102fee40d2522c426bdd63601f673fed0ca
AkhilVeeramalla <aveeramalla45@gmail.com>
2025-09-09 23:28:47 +0530

    ViewChild ViewChildren decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, QueryList, viewChild, ViewChild, ViewChildren } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CourseCardComponent } from "./course-card/course-card.component";
 import {COURSES} from '../db-data';
@@ -10,9 +10,10 @@ import { Course } from './model/course';
   selector: 'app-root',
   imports: [RouterOutlet, CourseCardComponent,CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'angular-course';
 
 onCongratsClick(){
@@ -29,8 +30,45 @@ coreCourse = COURSES[0];
 rxjsCourse = COURSES[1];
 ngrxCourse = COURSES[2];
 
+@ViewChild('cardRef2', {read: ElementRef})
+card!: ElementRef;
+
+@ViewChild('courseImage')
+courseImage!: ElementRef;
+
+@ViewChildren(CourseCardComponent, {read: ElementRef})
+cards!: QueryList<ElementRef>;
+
+constructor(){
+  console.log("containerDiv" , this.card);
+}
+
+ngAfterViewInit(){
+
+this.cards.changes.subscribe(
+  cards => console.log(cards)
+);
+
+console.log(this.cards);
+
+}
+
 onCourseSelected(course:Course){
     console.log("app component - click event bubbled...", course);
+    console.log("courseimage",this.courseImage);
+}
+
+onCoursesEdited(){
+  this.courses.push(
+    {
+        id: 1,
+        description: "Angular Core Deep Dive",
+        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
+        longDescription: "A detailed walk-through of the most important part of Angular - the Core and Common modules",
+        category: 'INTERMEDIATE',
+        lessonsCount: 10
+    }
+  );
 }
 
 trackCourse(index:number, course:Course){
